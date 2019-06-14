4b2d875aad0e7545667f2623ecc8f8a5f2abf637
Alisson Schwanz <alissonludtkeschwanz@gmail.com>
2019-06-14 10:52:45 -0400

    add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const path = require("path");
 
 mongoose.connect(
@@ -14,6 +15,7 @@ mongoose.connect(
 
 const app = express();
 
+app.use(cors());
 // Static access to images
 app.use(
   "/files",
